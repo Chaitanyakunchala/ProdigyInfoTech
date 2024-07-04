4644845bcf13e313bd25641c382fdb21a6aeae00
Kunchala Chaitanya <157344839+Chaitanyakunchala@users.noreply.github.com>
2024-07-04 10:32:11 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    var navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) {
-        navbar.style.backgroundColor = '#333';
-    } else {
-        navbar.style.backgroundColor = 'transparent';
-    }
-});
